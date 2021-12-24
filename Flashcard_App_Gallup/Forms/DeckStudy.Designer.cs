1e085a6cda3e689893a0dc086f1c56d357037257
hansenben3 <59586435+hansenben3@users.noreply.github.com>
2021-12-24 13:40:10 -0600

    Created Study forms

--- Flashcard_App_Gallup/Forms/DeckStudy.Designer.cs
+++ Flashcard_App_Gallup/Forms/DeckStudy.Designer.cs
@@ -29,19 +29,73 @@ namespace Flashcard_App_Gallup.Forms
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.lbl_currcard = new System.Windows.Forms.Label();
+			this.lbl_slash = new System.Windows.Forms.Label();
+			this.lbl_cardct = new System.Windows.Forms.Label();
+			this.btn_exit = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// lbl_currcard
+			// 
+			this.lbl_currcard.AutoSize = true;
+			this.lbl_currcard.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_currcard.Location = new System.Drawing.Point(379, 9);
+			this.lbl_currcard.Name = "lbl_currcard";
+			this.lbl_currcard.Size = new System.Drawing.Size(32, 37);
+			this.lbl_currcard.TabIndex = 0;
+			this.lbl_currcard.Text = "1";
+			// 
+			// lbl_slash
+			// 
+			this.lbl_slash.AutoSize = true;
+			this.lbl_slash.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_slash.Location = new System.Drawing.Point(417, 9);
+			this.lbl_slash.Name = "lbl_slash";
+			this.lbl_slash.Size = new System.Drawing.Size(28, 37);
+			this.lbl_slash.TabIndex = 1;
+			this.lbl_slash.Text = "/";
+			// 
+			// lbl_cardct
+			// 
+			this.lbl_cardct.AutoSize = true;
+			this.lbl_cardct.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_cardct.Location = new System.Drawing.Point(451, 9);
+			this.lbl_cardct.Name = "lbl_cardct";
+			this.lbl_cardct.Size = new System.Drawing.Size(32, 37);
+			this.lbl_cardct.TabIndex = 2;
+			this.lbl_cardct.Text = "5";
+			// 
+			// btn_exit
+			// 
+			this.btn_exit.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.btn_exit.Location = new System.Drawing.Point(13, 9);
+			this.btn_exit.Name = "btn_exit";
+			this.btn_exit.Size = new System.Drawing.Size(75, 37);
+			this.btn_exit.TabIndex = 3;
+			this.btn_exit.Text = "Exit";
+			this.btn_exit.UseVisualStyleBackColor = true;
 			// 
 			// DeckStudy
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(884, 711);
+			this.Controls.Add(this.btn_exit);
+			this.Controls.Add(this.lbl_cardct);
+			this.Controls.Add(this.lbl_slash);
+			this.Controls.Add(this.lbl_currcard);
 			this.Name = "DeckStudy";
 			this.Text = "Flashcard App";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label lbl_currcard;
+		private System.Windows.Forms.Label lbl_slash;
+		private System.Windows.Forms.Label lbl_cardct;
+		private System.Windows.Forms.Button btn_exit;
 	}
 }
---
+++ Flashcard_App_Gallup/Prefabs/StudyDeckPrefab.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Flashcard_App_Gallup.Prefabs
+{
+	partial class StudyDeckPrefab
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btn_flip = new System.Windows.Forms.Button();
+			this.btn_next = new System.Windows.Forms.Button();
+			this.txt_cardtxt = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// btn_flip
+			// 
+			this.btn_flip.Location = new System.Drawing.Point(70, 182);
+			this.btn_flip.Name = "btn_flip";
+			this.btn_flip.Size = new System.Drawing.Size(111, 42);
+			this.btn_flip.TabIndex = 1;
+			this.btn_flip.Text = "Flip";
+			this.btn_flip.UseVisualStyleBackColor = true;
+			// 
+			// btn_next
+			// 
+			this.btn_next.Location = new System.Drawing.Point(396, 182);
+			this.btn_next.Name = "btn_next";
+			this.btn_next.Size = new System.Drawing.Size(110, 42);
+			this.btn_next.TabIndex = 2;
+			this.btn_next.Text = "Next";
+			this.btn_next.UseVisualStyleBackColor = true;
+			// 
+			// txt_cardtxt
+			// 
+			this.txt_cardtxt.DetectUrls = false;
+			this.txt_cardtxt.Enabled = false;
+			this.txt_cardtxt.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.txt_cardtxt.Location = new System.Drawing.Point(70, 30);
+			this.txt_cardtxt.Name = "txt_cardtxt";
+			this.txt_cardtxt.Size = new System.Drawing.Size(436, 123);
+			this.txt_cardtxt.TabIndex = 3;
+			this.txt_cardtxt.Text = "Card";
+			// 
+			// StudyDeckPrefab
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.txt_cardtxt);
+			this.Controls.Add(this.btn_next);
+			this.Controls.Add(this.btn_flip);
+			this.Name = "StudyDeckPrefab";
+			this.Size = new System.Drawing.Size(600, 250);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btn_flip;
+		private System.Windows.Forms.Button btn_next;
+		private System.Windows.Forms.RichTextBox txt_cardtxt;
+	}
+}
